70a0bf3b9daec2dcda9820557ed86284e5e9c1ad
Pirooz Wallace <PiroozWallace@outlook.com>
2021-01-06 14:54:46 -0800

    Create connection.js
    make the basic connection file to mysql

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+
+// connection file
+
+const mysql = require("mysql")
+let connection
+
+connection = mysql.createConnection({
+
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "omnicode",
+    database: "burgers_db",
+});
+
+connection.connect(function(err) {
+    if (err) throw (err);
+});
+
+module.exports = connection
